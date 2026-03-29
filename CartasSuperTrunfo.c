6f66a5fd2ce2ae224589dfa3e2a2fb4940d10446
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2026-03-29 18:07:46 +0000

    correcao scanf que estava digitado errado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,38 +6,35 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-    int     ncarta;
-    char    estado;
-    char    cod_carta[3];
-    char    nome_cidade[10];
-    int     populacao;
-    float   area;
-    float   pib;
-    int     p_turisticos;
+    int     ncarta, populacao, p_turisticos;
+    char    estado, cod_carta[3], nome_cidade[10];
+    float   area, pib;
+
   // Área para entrada de dados
     printf("Insira o número da carta:");
-    scarnf("%i";&ncarta);
-    printf("Insira o Estado (em letra maiúscula):");
-    scarnf(" %c";&estado);
+    scanf("%i",&ncarta);
+    printf("Insira o Estado _em letra maiúscula_:");
+    scanf(" %c",&estado);
     printf("Insira o nome da cidade:");
-    scarnf(" %s";nome_cidade);
+    scanf(" %s",nome_cidade);
     printf("Insira o tamanho da populacao:");
-    scarnf("%i";&populacao);
+    scanf("%i",&populacao);
     printf("Insira a area da cidade:");
-    scarnf("%f";&area);
+    scanf("%f",&area);
     printf("Insira o pib da cidade:");
-    scarnf("%f";&pib);
+    scanf("%f",&pib);
     printf("Insira a quantidade de pontos turisticos da cidade:");
-    scarnf("%i";&p_turisticos);
+    scanf("%i",&p_turisticos);
+        
   // Área para exibição dos dados da cidade
-    printf("CARTA",ncarta\n);
-    printf("ESTADO: ",estado\n);
-    printf("CODIGO: ",estado,ncarta\n);
-    printf("NOME DA CIDADE: ",nome_cidade);
-    printf("POPULACAO: ",populacao\n);
-    printf("AREA: ",area\n);
-    printf("PIB: ",pib\n);
-    printf("NUMERO DE PONTOS TURISTICOS: ",p_turisticos)
-    
-return 0;
+    printf("CARTA ncarta\n");
+    printf("ESTADO: estado\n");
+    printf("CODIGO: estado,ncarta\n");
+    printf("NOME DA CIDADE: nome_cidade\n");
+    printf("POPULACAO: populacao\n");
+    printf("AREA: area\n");
+    printf("PIB: pib\n");
+    printf("NUMERO DE PONTOS TURISTICOS: p_turisticos");
+
+  return 0;
 } 

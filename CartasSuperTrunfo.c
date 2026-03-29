6e45fff9732374e5f9e0edd32c3a3f0c7953faf9
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2026-03-29 18:12:16 +0000

    agora falta somente corrigir a exibição dos dados das cartas, demais pontos rodando bem, scanf e printf da parte de leitura de dados totalmente corrigida

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,16 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
     int     ncarta, populacao, p_turisticos;
-    char    estado, cod_carta[3], nome_cidade[10];
+    char    estado[2], cod_carta[3], nome_cidade[10];
     float   area, pib;
 
   // Área para entrada de dados
     printf("Insira o número da carta:");
     scanf("%i",&ncarta);
     printf("Insira o Estado _em letra maiúscula_:");
-    scanf(" %c",&estado);
+    scanf("%s",&estado);
     printf("Insira o nome da cidade:");
-    scanf(" %s",nome_cidade);
+    scanf("%s",nome_cidade);
     printf("Insira o tamanho da populacao:");
     scanf("%i",&populacao);
     printf("Insira a area da cidade:");
@@ -27,7 +27,7 @@ int main() {
     scanf("%i",&p_turisticos);
         
   // Área para exibição dos dados da cidade
-    printf("CARTA ncarta\n");
+    printf("CARTA: ncarta\n");
     printf("ESTADO: estado\n");
     printf("CODIGO: estado,ncarta\n");
     printf("NOME DA CIDADE: nome_cidade\n");
